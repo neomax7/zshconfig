849a5b2eebc7d9a5362f9e2f48dd9fe1c4d13e76
jjong <jjong84@gmail.com>
2022-11-27 00:49:52 +0900

    typo fixed

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-cd '${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k' && git pull
-cd '${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/' && git pull
-cd '${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/' && git pull
-cd '${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-completions/' && git pull
-cd '${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-history-substring-search/' && git pull
+cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}'/themes/powerlevel10k' && git pull
+cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}'/plugins/zsh-syntax-highlighting/' && git pull
+cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}'/plugins/zsh-autosuggestions/' && git pull
+cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}'/plugins/zsh-completions/' && git pull
+cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}'/plugins/zsh-history-substring-search/' && git pull
 
 
