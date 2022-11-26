354d8c721cd317cf04034925806581c6b0deed15
jjong <jjong84@gmail.com>
2022-11-26 23:53:12 +0900

    initial

---
+++ install.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#### sudo check
+
+#### things to do ####
+#### if /bin/zsh || /usr/bin/zsh not exist
+#### install zsh
+
+#### chsh -s /usr/bin/zsh $(whoami)
+mkdir ~/.fonts
+cp -f ./hack/* ~/.fonts/
+fc-cache -fv
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/
+git clone --depth=1 https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-completions/
+git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-history-substring-search/
+
+
