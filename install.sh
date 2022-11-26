af4e9b200558fcf33586f2ccf64a18350e5bd05a
jjong <jjong84@gmail.com>
2022-11-27 00:02:09 +0900

    initial

--- install.sh
+++ install.sh
@@ -7,12 +7,8 @@
 #### install zsh
 
 #### chsh -s /usr/bin/zsh $(whoami)
-mkdir ~/.fonts
-cp -f ./hack/* ~/.fonts/
-fc-cache -fv
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/
