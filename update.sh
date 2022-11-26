cff39febd3cf7f426754d7e81cc047dcbc8fd0d4
jjong <jjong84@gmail.com>
2022-11-27 00:22:40 +0900

    added update.sh

---
+++ update.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git pull
+cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/
+git pull
+cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/
+git pull
+cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-completions/
+git pull
+cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-history-substring-search/
+git pull
+
+
